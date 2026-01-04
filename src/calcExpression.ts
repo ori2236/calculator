300ccc49b4fad0dfe7a719a9be9b8093f1e5fc5b
Ori Levi <orile03@gmail.com>
2026-01-04 15:18:08 +0200

    validBrackets function

--- src/calcExpression.ts
+++ src/calcExpression.ts
@@ -4,7 +4,23 @@ export type IsValidType = {
 };
 
 export const validBrackets = (expression: string): boolean => {
-  return false;
+  const expressionArray = [...expression];
+  try {
+    const counter = expressionArray.reduce((counter, char) => {
+      if (char === "(") counter++;
+      else if (char === ")") {
+        counter--;
+        if (counter < 0) throw new Error("invalid brackets");
+      }
+      return counter;
+    }, 0);
+
+    if (counter != 0) return false;
+    return true;
+    
+  } catch (e) {
+    return false;
+  }
 };
 
 export const validateExpression = (expression: string): IsValidType => {
